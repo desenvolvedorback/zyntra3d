2c9b2d335a2248268654ca507755d7f6784bb670
Davileonardo Maxel <davileonardomaxel346@gmail.com>
2025-12-16 14:43:44 +0000

    Console FirebaseError
    Firebase: Error (auth/api-key-not-valid.-please-

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBKFE8vARSAhRZm7uklWDD_Sl7nubk18uM",
+  authDomain: "doce-sabor-2f261.firebaseapp.com",
+  projectId: "doce-sabor-2f261",
+  storageBucket: "doce-sabor-2f261.appspot.com",
+  messagingSenderId: "91616889474",
+  appId: "1:91616889474:web:fb57f86b4de36eb159829b"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
