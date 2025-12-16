62894c72d1830a310b455b8a127c0d6da81761b3
Davileonardo Maxel <davileonardomaxel346@gmail.com>
2025-12-16 15:50:11 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBKFE8vARSAhRZm7uklWDD_Sl7nubk18uM",
+  apiKey: "AIzaSyAXcXl-yOuaSvg24vnFkw7ztFEK-YqNdFI",
   authDomain: "doce-sabor-2f261.firebaseapp.com",
   projectId: "doce-sabor-2f261",
   storageBucket: "doce-sabor-2f261.appspot.com",
   messagingSenderId: "91616889474",
-  appId: "1:91616889474:web:fb57f86b4de36eb159829b"
+  appId: "1:91616889474:web:fb57f86b4de36eb159829b",
+  measurementId: "G-NV30YBJE2J"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
